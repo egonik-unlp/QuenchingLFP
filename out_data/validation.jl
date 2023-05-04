ed103a443298cd8d92c58222eab75e118d76b85f
Eduardo Gonik <gonik@quimica.unlp.edu.ar>
2023-05-03 23:46:51 -0300

    nuevo peq fix

--- out_data/validation.jl
+++ out_data/validation.jl
@@ -1,5 +1,5 @@
 include("master.jl")
-using .LFPExperiment, StatsBase
+using .LFPExperiment, StatsBase, JLD
 const hdir = pwd()
 path = joinpath(hdir,"val")
 if !isdir("val")
@@ -12,19 +12,19 @@ println("Chosen exp = $(rexp.material) $(rexp.treatment)")
 A, I, T = rexp.ind, rexp.I, rexp.tau
 params_test = [A, I, T]
 LFPExperiment.fit(rexp)
-
+rtb = rexp.treatment
 params_ref = rexp.fit.param
 res = Vector{Any}()
 data = Dict()
-for multiplier ∈ .1:.1:1.9
+for multiplier ∈ .1:.1:10
 	println("$multiplier => ")
 	base_params = params_test * multiplier
 	rexp.ind, rexp.I, rexp.tau = base_params
-	rexp.treatment = "$(rexp.treatment)_val_$multiplier"
+	rexp.treatment = "$(rtb)_val_$multiplier"
 	LFPExperiment.fit(rexp)
 	LFPExperiment.plot(rexp; dir = path)
 	data[multiplier] = rexp.fit.param
 end
 
-
+@save "$path/dataval.jld" data
 
